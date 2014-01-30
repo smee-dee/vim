b01a4e22c87a2deb7a854e6c73053af889b92049
Stefan Schmidt <stefan.schmidt@kaeuferportal.de>
2014-01-30 21:45:57 +0100

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -4,4 +4,4 @@ rm -fr ~/.vim 2>/dev/null
 rm -f ~/.vimrc 2>/dev/null
 rvm use system
 brew install macvim --override-system-vim --enable-clipboard
-git clone git@github.com:stefanschmidt83/vim.git ~/.vim && cd ~/.vim && vim -c BundleInstall! -c q -c q -u bundles.vim && ln -s ~/.vim/vimrc ~/.vimrc
+git clone git@github.com:smee-dee/vim.git ~/.vim && cd ~/.vim && vim -c BundleInstall! -c q -c q -u bundles.vim && ln -s ~/.vim/vimrc ~/.vimrc
